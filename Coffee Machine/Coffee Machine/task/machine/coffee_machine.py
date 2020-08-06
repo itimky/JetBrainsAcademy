921d76e203ad73948a1fd642cb35e28466e4dbb1
Ilia.Yusov <ouster2k@gmail.com>
2020-08-06 19:00:37 +0300

    more coffee

--- Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
@@ -1,1 +1,5 @@
-# Write your code here
+cups = int(input('Write how many cups of coffee you will need: '))
+print('For', str(cups), 'cups of coffee you will need:')
+print(cups * 200, 'ml of water')
+print(cups * 50, 'ml of milk')
+print(cups * 15, 'g of coffee beans')
