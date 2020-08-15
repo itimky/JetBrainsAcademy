1ddb86265cdd92b9b8f3a2b15f65ae214435c28a
Ilia.Yusov <ouster2k@gmail.com>
2020-08-15 18:28:53 +0300

    rock_paper_scissors

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,33 +1,65 @@
 import random
 
-name = input('Enter your name: ')
-print('Hello,', name)
 
-score = 0
-f = open('rating.txt')
-for line in f:
-    if line.split()[0] == name:
-        score = int(line.split()[1])
-
-winning_moves = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
-loosing_moves = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
-
-player_hand = ''
-while True:
-    player_hand = input()
-    computer_hand = random.choice(list(winning_moves.keys()))
-    if player_hand == '!exit':
-        print('Bye!')
-        exit()
-    elif player_hand == winning_moves[computer_hand]:
-        print('Well done. The computer chose {} and failed'.format(computer_hand))
-        score += 100
-    elif player_hand == loosing_moves[computer_hand]:
-        print('Sorry, but the computer chose {}'.format(computer_hand))
-    elif player_hand == computer_hand:
-        print('There is a draw {}'.format(computer_hand))
-        score += 50
-    elif player_hand == '!rating':
-        print('Your rating:', score)
+def win(first_move, second_move, options):
+    if first_move in options and second_move in options:
+        if first_move == second_move:
+            return 'draw'
+        else:
+            precedence = options[options.index(first_move) + 1:] + options[:options.index(first_move)]
+            if precedence.index(second_move) + 1 > len(precedence) / 2:
+                return 'first'
+            else:
+                return 'second'
     else:
-        print('Invalid input')
+        print('Move not in options')
+        return None
+
+
+def pc_move(options):
+    return random.choice(options)
+
+
+def main():
+    name = input('Enter your name: ')
+    print('Hello,', name)
+    score = 0
+    f = open('rating.txt')
+    for line in f:
+        if line.split()[0] == name:
+            score = int(line.split()[1])
+    f.close()
+
+    options = input().split(',')
+    if options == ['']:
+        options = ['rock', 'paper', 'scissors']
+
+    print("Okay, let's start")
+    while True:
+        command = input()
+        pc_hand = pc_move(options)
+
+        if command[0] == '!':
+            if command == '!exit':
+                print('Bye!')
+                exit()
+            elif command == '!rating':
+                print('Your rating:', score)
+            else:
+                print('Invalid input')
+        else:
+            winner = win(command, pc_hand, options)
+            if winner == 'first':
+                print('Well done. The computer chose {} and failed'.format(pc_hand))
+                score += 100
+            elif winner == 'second':
+                print('Sorry, but the computer chose {}'.format(pc_hand))
+            elif winner == 'draw':
+                print('There is a draw {}'.format(pc_hand))
+                score += 50
+            else:
+                print('Invalid input')
+
+
+if __name__ == '__main__':
+    main()
