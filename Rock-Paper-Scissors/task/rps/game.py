b0ae6968d35c90f80d9bb6f910e41e3c300853d3
Ilia.Yusov <ouster2k@gmail.com>
2020-08-15 16:54:14 +0300

    rock_paper_scissors

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,3 +1,33 @@
-move = input()
-answer = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
-print('Sorry, but the computer chose {}'.format(answer[move]))
+import random
+
+name = input('Enter your name: ')
+print('Hello,', name)
+
+score = 0
+f = open('rating.txt')
+for line in f:
+    if line.split()[0] == name:
+        score = int(line.split()[1])
+
+winning_moves = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+loosing_moves = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
+
+player_hand = ''
+while True:
+    player_hand = input()
+    computer_hand = random.choice(list(winning_moves.keys()))
+    if player_hand == '!exit':
+        print('Bye!')
+        exit()
+    elif player_hand == winning_moves[computer_hand]:
+        print('Well done. The computer chose {} and failed'.format(computer_hand))
+        score += 100
+    elif player_hand == loosing_moves[computer_hand]:
+        print('Sorry, but the computer chose {}'.format(computer_hand))
+    elif player_hand == computer_hand:
+        print('There is a draw {}'.format(computer_hand))
+        score += 50
+    elif player_hand == '!rating':
+        print('Your rating:', score)
+    else:
+        print('Invalid input')
