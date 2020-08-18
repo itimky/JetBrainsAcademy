48be6509afee3e04de95abdd04459a85a5b12e71
Ilia.Yusov <ouster2k@gmail.com>
2020-08-18 23:21:16 +0300

    simple banking system

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,4 +1,6 @@
 import random
+import sqlite3
+import string
 
 
 def luhn(card_without_checksum):
@@ -10,52 +12,112 @@ def luhn(card_without_checksum):
     return '0' if checksum == 10 else str(checksum)
 
 
-def create_account():
+def create_account(connection):
     bank_id = '400000'
-    acc_id = ''.join([str(random.randint(0, 9)) for _ in range(9)])
+    acc_id = ''.join(random.sample(string.digits, 9))
     checksum = luhn(bank_id + acc_id)
     card_number = bank_id + acc_id + checksum
-    pin = ''.join([str(random.randint(0, 9)) for _ in range(4)])
-    if card_number not in cards:
-        cards[card_number] = pin
-        balance[card_number] = 0
+    pin = ''.join(random.sample(string.digits, 4))
+    cursor = connection.cursor()
+    cursor.execute('SELECT number FROM card')
+    if (card_number,) not in cursor.fetchall():
+        cursor.execute(f"INSERT INTO card (number, pin) VALUES ('{card_number}', '{pin}')")
+        connection.commit()
         print('\nYour card has been created\n'
               'Your card number:\n' + card_number + '\n'
               'Your card PIN:\n' + pin + '\n')
     else:
         print('Card already exist\n')
+    cursor.close()
+    return None
 
 
-def login():
+def login(connection):
     user_card = input('\nEnter your card number:\n')
     user_pin = input('Enter your PIN:\n')
-    if user_card in cards and user_pin == cards[user_card]:
-        print('\nYou have successfully logged in!\n')
-        return {'state': 'logged', 'id': user_card}
+    if user_card.isdigit() and user_pin.isdigit():
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT id, number, pin FROM card WHERE number = '{user_card}'")
+        data = cursor.fetchone()
+        cursor.close()
+        if data:
+            db_id, db_card, db_pin = data
+            if db_pin == user_pin:
+                print('\nYou have successfully logged in!\n')
+                return {'state': 'logged', 'id': db_id}
     print('\nWrong card number or PIN!\n')
     return {'state': 'main', 'id': 0}
 
 
-qwe = ''.join([str(random.randint(0, 9)) for _ in range(9)])
-print(qwe)
+def get_balance(connection, current):
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT balance FROM card WHERE id = {current["id"]}')
+    balance = cursor.fetchone()[0]
+    return balance
 
 
-cards = {}
-balance = {}
+def create_table(connection):
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS card ('
+                   'id INTEGER PRIMARY KEY, '
+                   'number TEXT, '
+                   'pin TEXT, '
+                   'balance INTEGER DEFAULT 0)'
+                   ';')
+    connection.commit()
+    cursor.close()
+    return None
+
+
+def add_income(connection, current):
+    income = int(input('\nEnter income:\n'))
+    cursor = connection.cursor()
+    cursor.execute(f'UPDATE card SET balance = balance + {income} WHERE id = {current["id"]}')
+    connection.commit()
+    print('Income was added!\n')
+    cursor.close()
+    return None
+
+
+# def print_card(connection):
+#     cursor = connection.cursor()
+#     cursor.execute('SELECT number FROM card')
+#     print(cursor.fetchall())
+#     cursor.close()
+
+
+conn = sqlite3.connect('card.s3db')
+create_table(conn)
 cmd = ''
-current = {'state': 'main', 'id': 0}
-menu = {'main': '1. Create an account\n2. Log into account\n0. Exit\n',
-        'logged': '1. Balance\n2. Log out\n0. Exit\n'}
+curr = {'state': 'main', 'id': 0}
+menu = {'main': '1. Create an account\n'
+                '2. Log into account\n'
+                '0. Exit\n',
+        'logged': '1. Balance\n'
+                  '2. Add income\n'
+                  '3. Do transfer\n'
+                  '4. Close account\n'
+                  '5. Log out\n'
+                  '0. Exit\n'}
 while cmd != '0':
-    cmd = input(menu[current['state']])
-    if current['state'] == 'main':
+    cmd = input(menu[curr['state']])
+    if curr['state'] == 'main':
         if cmd == '1':
-            create_account()
+            create_account(conn)
         elif cmd == '2':
-            current = login()
-    elif current['state'] == 'logged':
+            curr = login(conn)
+        # elif cmd == '3':
+        #     print_card(conn)
+    elif curr['state'] == 'logged':
         if cmd == '1':
-            print('\nBalance:', balance[current['id']], '\n')
+            print('\nBalance:', get_balance(conn, curr), '\n')
         elif cmd == '2':
+            add_income(conn, curr)
+        elif cmd == '3':
+            pass
+        elif cmd == '4':
+            pass
+        elif cmd == '5':
             print('\nYou have successfully logged out!\n')
-            current['state'] = 'main'
+            curr = {'state': 'main', 'id': 0}
+conn.close()
